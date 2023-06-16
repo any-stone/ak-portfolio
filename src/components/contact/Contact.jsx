c2cefc9c0c2b0cc2a1ca8b529d8a70d3339605e9
Annie Kuxa <kuxa.business@gmail.com>
2023-06-16 14:15:14 -0300

    implement contact form sending messages functionality

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './contact.css'
-import {MdOutlineEmail} from 'react-icons/md'
-import {RiMessengerLine} from 'react-icons/ri'
+import { MdOutlineEmail } from 'react-icons/md'
+import { RiMessengerLine } from 'react-icons/ri'
+import emailjs from 'emailjs-com'
+
 
 const Contact = () => {
+  const form = useRef()
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_bbha3xc', 'template_wn33z9n', form.current, 'E9Qd9hRrOgRL5cQ8Z')
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+
+      e.target.reset()
+  };
+
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -12,24 +29,24 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
-            <MdOutlineEmail className='contact__option-icon'/>
+            <MdOutlineEmail className='contact__option-icon' />
             <h4>Email</h4>
             <h5>kuxa.business@gmail.com</h5>
             <a href="mailto:kuxa.business@gmail.com" target='_blank'>Send a message</a>
           </article>
           <article className="contact__option">
-            <RiMessengerLine className='contact__option-icon'/>
+            <RiMessengerLine className='contact__option-icon' />
             <h4>Messenger</h4>
             <h5>Annie Kuksa</h5>
             <a href="https://m.me/so.annett" target='_blank'>Send a message</a>
           </article>
         </div>
 
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name='name' placeholder='Your Full Name' required />
           <input type="email" name='email' placeholder='Your Email' required />
           <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
-          <button type='submit'className='btn btn-primary'>Send Message</button>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
         </form>
 
       </div>
